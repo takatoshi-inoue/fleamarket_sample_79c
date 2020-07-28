fc863e87271427c8d2901d011faa1f441226ec3f
takatoshi-inoue <67253927+takatoshi-inoue@users.noreply.github.com>
2020-07-28 10:59:46 +0900

    テスト show

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :posts, only: [:new,]
+  resources :posts, only: [:new]
+  resources :users, only: [:show]
 
 end
