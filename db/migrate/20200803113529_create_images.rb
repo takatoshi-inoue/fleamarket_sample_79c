e11e0330a49b34d932e59dbd7bd6847db7b26cc9
jin0227soccer <jinsoccer0520@gmail.com>
2020-08-04 11:30:33 +0900

    修正

--- db/migrate/20200803113529_create_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateImages < ActiveRecord::Migration[6.0]
-  def change
-    create_table :images do |t|
-      t.string :name, null: false
-      t.references :post, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
