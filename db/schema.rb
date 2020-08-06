b172aa039b2a7d8884e66b1901db2bf2a74f4cd3
rariruren555 <67192257+rariruren555@users.noreply.github.com>
2020-08-06 17:57:47 +0900

    Update master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_053734) do
+ActiveRecord::Schema.define(version: 2020_08_06_082108) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2020_08_06_053734) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "customer_id", null: false
     t.bigint "user_id"
@@ -30,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_053734) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2020_08_06_053734) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["ancestry"], name: "index_categories_on_ancestry"
-
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
