24b03e66526808343196f1c9acc95065509212b3
takatoshi-inoue <67253927+takatoshi-inoue@users.noreply.github.com>
2020-08-06 22:11:47 +0900

    コミットtable

--- db/migrate/20200728075530_create_posts.rb
+++ db/migrate/20200728075530_create_posts.rb
@@ -3,14 +3,14 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.string :name, null: false
       t.text :text, null: false
-      t.string :condition, null: false
-      t.string :burden, null: false
-      t.string :area, null: false
-      t.string :day, null: false
+      t.string :condition, null: false, default: 0
+      t.string :burden, null: false, default: 0
+      t.string :area, null: false, default: 0
+      t.string :day, null: false, default: 0
       t.string :price, null: false
       t.references :user, null: false, foreign_key: true
       t.string :brand, null: false
-
+      t.integer :buyer_id
       t.timestamps
     end
     add_index :posts, :name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_08_06_082108) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
