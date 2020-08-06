b01a67da7f8d35a131d595b3db448584b65904be
takatoshi-inoue <67253927+takatoshi-inoue@users.noreply.github.com>
2020-08-06 18:22:28 +0900

    コミット７

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  before_action :item_sold?, only: [:show]
+
   def index
     @posts = Post.includes(:images).order('created_at DESC')
     
   end
   
   def show
-  
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -21,6 +24,12 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def item_sold?
+    redirect_to action: :index if Post.find(params[:id]).brand == "b"
   end
 
 end
