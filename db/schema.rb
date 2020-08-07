e2317644dd68f105c9c3d7dc9eb318b299aebe8d
rariruren555 <67192257+rariruren555@users.noreply.github.com>
2020-08-07 19:24:39 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_08_07_093740) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
-    t.integer "prefecture", default: 0, null: false
+    t.integer "prefecture", null: false
     t.string "city", null: false
     t.string "address", null: false
     t.string "apartment"
