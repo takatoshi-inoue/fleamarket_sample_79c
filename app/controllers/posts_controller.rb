1d0a53c0de56b07b1c39abda4b5cd4c40fe51b3c
sheep1755 <the_straw_hat@icloud.com>
2020-08-10 19:45:39 +0900

    Edit-post-information1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,11 +31,27 @@ class PostsController < ApplicationController
       render :show
     end  
   end
+
+  def edit
+    @post = Post.find(params[:id])
+    @post.images.new
+    # @post = Post.new
+    # @post.images.new
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path  
+    else
+      render :edit
+    end
+  end  
   
   private
   
   def post_params
-    params.require(:post).permit(:name ,:text, :condition, :burden, :area, :day, :price, :brand, images_attributes: [:name]).merge(user_id: current_user.id)
+    params.require(:post).permit(:name ,:text, :condition, :burden, :area, :day, :price, :brand, images_attributes: [:name, :_destroy, :id]).merge(user_id: current_user.id)
   end
 end
 
