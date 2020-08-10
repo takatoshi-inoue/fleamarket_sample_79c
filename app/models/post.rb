c45c0016d8eb6603c1895b5941a9d596c7953b77
jin0227soccer <jinsoccer0520@gmail.com>
2020-08-10 15:58:12 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
 
 
   belongs_to :user
-  belongs_to :category, dependent: :destroy
+  belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
